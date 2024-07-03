b6ee1a188ac177b62e17908c98424f2f4418b6d1
malekrezgui <rezguimalek51@gmail.com>
2024-07-03 16:57:07 +0100

    update .normal fix

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,14 +1,10 @@
 extends CharacterBody2D
 
-
 var speed = 600
-#var velocity = Vector2.ZERO
 
 func _ready():
 	initialize_ball_direction()
 	
-
-
 func initialize_ball_direction():
 	randomize()
 	var x_array = [-1,1]
@@ -17,22 +13,15 @@ func initialize_ball_direction():
 	velocity.y = y_array[randi()%2]
 	
 func _physics_process(delta):
-	var colliding = move_and_collide(velocity * speed * delta)
-	if colliding:
-		velocity = velocity.bounce(colliding.normal)
-		_send_ball_position(position)
-		
+	var collision = move_and_collide(velocity * speed * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+
 func stop_ball():
 	speed=0
 	
 func start_ball():
 	speed=600
 	initialize_ball_direction()
-	
-func _send_ball_position(pos):
-	ServerNetwork._update_ball_position(pos)
-	
-	
-	
 	
 	
